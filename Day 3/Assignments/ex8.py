8ed4ef2cf854b985d3e12e9b3b2b63ec9934c2f3
harbinger-vinayak <vinayak.katawe@harbingergroup.com>
2019-02-01 18:15:16 +0530

    Remove comment Commit

--- Day 3/Assignments/ex8.py
+++ Day 3/Assignments/ex8.py
@@ -1,15 +1,20 @@
 """
-Wap to accept directory name from user and remove all the empty files, from that directory. (os.path.getsize)
+Wap to accept directory name from user and remove if it is modify 30days older and if size is 100kb
 """
 
 import os
+import time
+
 path = raw_input("Enter file name: ")
 for root, dir, files in os.walk(path):
     for filename in files:
         fsize = os.path.getsize(root + "\\" + filename)
-        if fsize <= 0:
+        fday = os.stat(root + "\\" + filename).st_mtime
+        now = time.time()
+
+        if fsize <= 100 and fday < now - 30 * 86400:
             os.remove(root + "\\" + filename)
             print "File removed."
         else:
-            print "No empty file found"
+            print "No file found"
     break
